1f24ef62fdac3bcad54f0656880dcd239a742d4d
09111998Nowto <73967457+09111998Nowto@users.noreply.github.com>
2020-12-26 09:39:11 +0700

    Add files via upload

---
+++ merge.java
@@ -0,0 +1,72 @@
+public class test {
+
+    public static <T extends Comparable<T>> void mergeSort(T[] data)
+
+    {
+        mergeSort(data, 0, data.length - 1);
+    }
+
+    /**
+     * Recursively sorts a range of objects in the specified array using the merge
+     * sort algorithm.
+     *
+     * @param data the array to be sorted
+     * @param min  the index of the first element
+     * @param max  the index of the last element
+     */
+    private static <T extends Comparable<T>> void mergeSort(T[] data, int min, int max) {
+        if (min < max) {
+            int mid = (min + max) / 2;
+            mergeSort(data, min, mid);
+            mergeSort(data, mid + 1, max);
+            merge(data, min, mid, max);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T extends Comparable<T>> void merge(T[] data, int first, int mid, int last) {
+        T[] temp = (T[]) (new Comparable[data.length]);
+        int first1 = first, last1 = mid; // endpoints of first subarray
+        int first2 = mid + 1, last2 = last; // endpoints of second subarray
+        int index = first1; // next index open in temp array
+        // Copy smaller item from each subarray into temp until one
+        // of the subarrays is exhausted
+        while (first1 <= last1 && first2 <= last2) {
+            if (data[first1].compareTo(data[first2]) < 0) {
+                temp[index] = data[first1];
+                first1++;
+            } else {
+                temp[index] = data[first2];
+                first2++;
+            }
+            index++;
+        }
+        // Copy remaining elements from first subarray, if any
+        while (first1 <= last1) {
+            temp[index] = data[first1];
+            first1++;
+            index++;
+        }
+        // Copy remaining elements from second subarray, if any
+        while (first2 <= last2) {
+            temp[index] = data[first2];
+            first2++;
+            index++;
+        }
+        // Copy merged data into original array
+        for (index = first; index <= last; index++)
+            data[index] = temp[index];
+    }
+    public static void main(String args[]) {
+        Integer arr[] = { 6, 5, 12, 10, 9, 1 };
+    
+        test ob = new test();
+        ob.mergeSort(arr);
+    
+        System.out.println("Sorted array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" "+arr[i]);
+        }
+      }
+    
+}
